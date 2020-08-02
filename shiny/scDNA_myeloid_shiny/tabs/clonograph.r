b277c491f36e00b711270b83e2da97a6baf202e0
Nayla <nayla.boorady@gmail.com>
2020-08-02 19:51:28 -0400

    Clonograph Changes

--- shiny/scDNA_myeloid_shiny/tabs/clonograph.r
+++ shiny/scDNA_myeloid_shiny/tabs/clonograph.r
@@ -1,7 +1,9 @@
 tabPanel(title = "Clonograph", 
          sidebarLayout(
            sidebarPanel(
-             selectInput("clonoInput", "Sample Names:", names(sample_list),multiple = TRUE,selected="MSK45")
+             selectInput("clonoInput", "Select Gene of Interest:", colnames(sample_list)[1:9],multiple = TRUE,selected="MSK45"),
+             selectInput("clonoInput", "Select Patients With Gene:", names(sample_list),multiple = TRUE,selected="MSK45")
+             
              
            ),
            mainPanel(
